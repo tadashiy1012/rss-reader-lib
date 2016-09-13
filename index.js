1c980c1c349edfe5dd6d2dff07f0485fbbad7ab8
tadashiy1012 <yama3ki.tada4@gmail.com>
2016-09-14 08:05:08 +0900

    modify

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 module.exports = (function() {
   const request = require('request');
   const parseString = require('xml2js').parseString;
-  function checkExt(url) {
-    const ext = url.substring(url.lastIndexOf('.') + 1);
-    if (ext === 'xml') {
+  function checkExt(obj) {
+    if (obj.hasOwnProperty('rss')) {
       return 'rss';
-    } else if (ext === 'atom') {
+    } else if (obj.hasOwnProperty('feed')) {
       return 'atom';
     } else {
       return 'n/a';
     }
   }
+  function replaceHtml(html) {
+    return html.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'');
+  }
   function convertRss(items) {
     const title = items.title;
     const entries = [];
     for (let entry of items.item) {
-      console.log(entry);
       entries.push({
-        title: entry.title,
-        link: entry.link,
-        content: entry.description
+        title: entry.title[0],
+        link: entry.link[0],
+        content: entry.description !== undefined ? 
+          replaceHtml(entry.description[0]) : null,
+        date: new Date(entry.pubDate)
       });
     }
     return [title, entries];
@@ -29,27 +32,27 @@ module.exports = (function() {
     const entries = [];
     for (let entry of items.entry) {
       entries.push({
-        title: entry.title,
-        link: entry.link,
-        content: entry.content
+        title: entry.title[0]._,
+        link: entry.link[0].$.href,
+        content: replaceHtml(entry.content[0]._),
+        date: new Date(entry.updated[0])
       });
     }
     return [title, entries];
   }
   return function(tgtUrl) {
     return new Promise((resolve, reject) => {
-      const ext = checkExt(tgtUrl);
-      if (ext === 'n/a') {
-        reject(new Error('incompatible error'));
-      }
       request(tgtUrl, (err, resp, body) => {
         if (err) { reject(err); }
         parseString(body, (err, result) => {
           if (err) { reject(err); }
+          const ext = checkExt(result);
           if (ext === 'rss') {
             resolve(convertRss(result.rss.channel[0]));
-          } else {
+          } else if (ext === 'atom') {
             resolve(convertAtom(result.feed));
+          } else {
+            reject(new Error('unsuported type'));
           }
         });
       });
